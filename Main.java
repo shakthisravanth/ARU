cb9c9a3686a2f689e99f7aec79168f9ff9195fae
J SHAKTHI SRAVANTH <121029982+shakthisravanth@users.noreply.github.com>
2026-01-19 17:54:57 +0530

    Add comments to Main class and main method

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("My Daily Plan App");
-    }
-}
+// Create a class named Main
+
+// Inside the class, write the main method
+
+// Inside the main method:
+// Print a message to the console
+
+// The message should be:
+// "My Daily Plan App"
